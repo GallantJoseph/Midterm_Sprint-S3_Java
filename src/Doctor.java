9f8d50a851d61097cecdf2c2e7c2a4c6f07c0bb4
GallantJoseph <gallant.joseph@outlook.com>
2025-06-17 12:15:39 -0230

    Doctor class updates
    -Comments
    -Getters and setters
    -Add a patient method

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,12 +1,42 @@
+/*
+    Description: Midterm Sprint - Semester 3 - Doctor Class
+    Authors:    Ashton Dennis
+                Justin Greenslade
+                Joseph Gallant
+    Dates: June 16, 2025 -
+ */
+
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Doctor extends Person {
+    // Private attributes
     private ArrayList<Patient> patients;
     private String specialization;
 
-    public Doctor(int id, String firstName, String lastName, int age, String phone, String specialization, ArrayList<Patient> patients) {
-        super(id, firstName, lastName, age, phone);
+    // Constructors
+    // Parameterized
+    public Doctor(int id, String firstName, String lastName, Date dateOfBirth, String phone, char gender, String specialization, ArrayList<Patient> patients) {
+        super(id, firstName, lastName, dateOfBirth, phone, gender);
         this.patients = new ArrayList<>(patients);
         this.specialization = specialization;
+    }
+
+    // Getters and setters
+    public ArrayList<Patient> getPatients() {
+        return patients;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    // Add a patient to a doctor
+    public void addPatient(Patient patient) {
+        this.patients.add(patient);
     }
 }
