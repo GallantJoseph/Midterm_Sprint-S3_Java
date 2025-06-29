0c98d09bbacac7a7993c8fb45d89616d22117710
TheLostMrJ <greenslade_12@hotmail.com>
2025-06-28 22:45:15 -0230

    Javadocs for medication and prescription
    Javadocs for medication and prescription

--- src/Medication.java
+++ src/Medication.java
@@ -18,8 +18,16 @@
      private LocalDate expiryDate;
  
      // Constructors
-     // Parameterized
-     public Medication (String name, double dose, int quantity, LocalDate expiryDate) {
+ 
+     /**
+      * Creates a new medication with the given information.
+      *
+      * @param name        The name of the medication.
+      * @param dose        The dosage amount (in mg).
+      * @param quantity    The number of units in stock.
+      * @param expiryDate  The date the medication expires.
+      */
+     public Medication(String name, double dose, int quantity, LocalDate expiryDate) {
          this.id = idCounter++;
          this.name = name;
          this.dose = dose;
@@ -27,7 +35,11 @@
          this.expiryDate = expiryDate;
      }
  
-     // Copy
+     /**
+      * Creates a copy of an existing medication.
+      *
+      * @param medication The medication object to copy.
+      */
      public Medication(Medication medication) {
          this.id = idCounter++;
          this.name = medication.name;
@@ -37,53 +49,109 @@
      }
  
      // Getters and Setters
+ 
+     /**
+      * Gets the name of the medication.
+      *
+      * @return The medication name.
+      */
      public String getName() {
          return name;
      }
-
+ 
+     /**
+      * Sets the name of the medication.
+      *
+      * @param name The new medication name.
+      */
      public void setName(String name) {
          this.name = name;
      }
  
+     /**
+      * Gets the dosage of the medication.
+      *
+      * @return The dosage amount in mg.
+      */
      public double getDose() {
          return dose;
      }
-
+ 
+     /**
+      * Sets the dosage of the medication.
+      *
+      * @param dose The new dosage amount in mg.
+      */
      public void setDose(double dose) {
          this.dose = dose;
      }
  
+     /**
+      * Gets the quantity of medication in stock.
+      *
+      * @return The quantity in stock.
+      */
      public int getQuantity() {
          return quantity;
      }
-
+ 
+     /**
+      * Sets the quantity of medication in stock.
+      *
+      * @param quantity The new quantity in stock.
+      */
      public void setQuantity(int quantity) {
          this.quantity = quantity;
      }
-
+ 
+     /**
+      * Adds more medication to the current quantity.
+      *
+      * @param value The amount to add to the stock.
+      */
      public void addQuantity(int value) {
-         this.quantity+=value;
+         this.quantity += value;
      }
  
+     /**
+      * Gets the expiry date of the medication.
+      *
+      * @return The expiry date.
+      */
      public LocalDate getExpiryDate() {
          return expiryDate;
      }
-
+ 
+     /**
+      * Sets the expiry date of the medication.
+      *
+      * @param expiryDate The new expiry date.
+      */
      public void setExpiryDate(LocalDate expiryDate) {
          this.expiryDate = expiryDate;
      }
-
+ 
+     /**
+      * Gets the unique ID for this medication.
+      *
+      * @return The medication ID.
+      */
      public int getId() {
          return this.id;
      }
-     
+ 
+     /**
+      * Returns a string with the medication details.
+      *
+      * @return A formatted string with medication info.
+      */
      @Override
-    public String toString() {
-        return "Medication ID: " + id +
-            "\nName: " + name +
-            "\nDosage: " + dose +
-            "\nQuantity in stock: " + quantity +
-            "\nExpiry Date: " + expiryDate;
-    }
-
+     public String toString() {
+         return "Medication ID: " + id +
+             "\nName: " + name +
+             "\nDosage: " + dose +
+             "\nQuantity in stock: " + quantity +
+             "\nExpiry Date: " + expiryDate;
+     }
  }
+ 
